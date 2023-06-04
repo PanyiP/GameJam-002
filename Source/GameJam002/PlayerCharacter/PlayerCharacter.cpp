fec0a6e0cb2e23069d7d2f254fbf945567f320f3
Peter Panyi <52467332+PanyiP@users.noreply.github.com>
2023-06-04 09:48:49 +0200

    Added attack input and animation

--- Source/GameJam002/PlayerCharacter/PlayerCharacter.cpp
+++ Source/GameJam002/PlayerCharacter/PlayerCharacter.cpp
@@ -1,3 +1,4 @@
+#include "PlayerCharacter.h"
 #include "Camera/CameraComponent.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
@@ -5,11 +6,10 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "InputMappingContext.h"
 #include "PaperFlipbookComponent.h"
-#include "PlayerCharacter.h"
 
 APlayerCharacter::APlayerCharacter()
 {
-	PrimaryActorTick.bCanEverTick = true;
+   PrimaryActorTick.bCanEverTick = true;
 
    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(FName(TEXT("CameraBoom")));
    CameraBoom->SetupAttachment(GetRootComponent());
@@ -21,7 +21,7 @@ APlayerCharacter::APlayerCharacter()
 
 void APlayerCharacter::BeginPlay()
 {
-	Super::BeginPlay();
+   Super::BeginPlay();
 
    if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
    {
@@ -37,23 +37,27 @@ void APlayerCharacter::BeginPlay()
 
 void APlayerCharacter::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+   Super::Tick(DeltaTime);
 
+   IsMoving();
    UpdateAnimation();
 }
 
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+   Super::SetupPlayerInputComponent(PlayerInputComponent);
 
    if (UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent))
    {
       Input->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+      Input->BindAction(AttackAction, ETriggerEvent::Triggered, this, &ThisClass::Attack);
    }
 }
 
 void APlayerCharacter::Move(const FInputActionValue& Value)
 {
+   if (CharacterState == ECharacterState::ECS_Attacking) return;
+
    AddMovementInput(FVector(Value.Get<FVector2D>().X, Value.Get<FVector2D>().Y, 0.f));
 
    if (Value.Get<FVector2D>().X > 0.f)
@@ -66,24 +70,73 @@ void APlayerCharacter::Move(const FInputActionValue& Value)
    }
 }
 
+void APlayerCharacter::Attack(const FInputActionValue& Value)
+{
+   if (CharacterState != ECharacterState::ECS_Attacking)
+   {
+      CharacterState = ECharacterState::ECS_Attacking;
+      GetSprite()->SetFlipbook(AttackAnimations[FMath::RandRange(0, AttackAnimations.Num() - 1)]);
+      GetSprite()->OnFinishedPlaying.AddDynamic(this, &ThisClass::AttackFinished);
+   }
+}
+
+void APlayerCharacter::AttackFinished()
+{
+   GetSprite()->OnFinishedPlaying.RemoveDynamic(this, &ThisClass::AttackFinished);
+   CharacterState = ECharacterState::ECS_Idle;
+}
+
 void APlayerCharacter::UpdateAnimation()
 {
-   if (GetMovementComponent() && GetMovementComponent()->Velocity.Length() > 0.f)
+   switch (CharacterState)
    {
+   case ECharacterState::ECS_Idle:
+      if (IdleAnimation)
+      {
+         GetSprite()->SetLooping(true);
+         GetSprite()->SetFlipbook(IdleAnimation);
+         if (!GetSprite()->IsPlaying()) GetSprite()->Play();
+      }
+      break;
+   case ECharacterState::ECS_Running:
+      GetSprite()->SetLooping(true);
       switch (CharacterDirection)
       {
       case ECharacterDirection::ECD_Left:
          if (RunLeftAnimation) GetSprite()->SetFlipbook(RunLeftAnimation);
+         if (!GetSprite()->IsPlaying()) GetSprite()->Play();
          break;
       case ECharacterDirection::ECD_Right:
          if (RunRightAnimation) GetSprite()->SetFlipbook(RunRightAnimation);
+         if (!GetSprite()->IsPlaying()) GetSprite()->Play();
          break;
       default:
          break;
       }
+      break;
+   case ECharacterState::ECS_Attacking:
+      if (!AttackAnimations.IsEmpty())
+      {
+         CharacterState = ECharacterState::ECS_Attacking;
+         GetSprite()->SetLooping(false);
+         if (!GetSprite()->IsPlaying()) GetSprite()->Play();
+      }
+      break;
+   default:
+      break;
+   }
+}
+
+void APlayerCharacter::IsMoving()
+{
+   if (CharacterState == ECharacterState::ECS_Attacking) return;
+
+   if (GetMovementComponent() && GetMovementComponent()->Velocity.Length() > 0.f)
+   {
+      CharacterState = ECharacterState::ECS_Running;
    }
    else
    {
-      if (IdleAnimation) GetSprite()->SetFlipbook(IdleAnimation);
+      CharacterState = ECharacterState::ECS_Idle;
    }
 }
--- Source/GameJam002/PlayerCharacter/PlayerCharacter.h
+++ Source/GameJam002/PlayerCharacter/PlayerCharacter.h
@@ -19,6 +19,15 @@ enum class ECharacterDirection
 	ECD_MAX
 };
 
+UENUM()
+enum class ECharacterState
+{
+	ECS_Idle = 0,
+	ECS_Running,
+	ECS_Attacking,
+	ECD_MAX
+};
+
 UCLASS()
 class GAMEJAM002_API APlayerCharacter : public APaperCharacter
 {
@@ -33,6 +42,9 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	/*
+	* Input
+	*/
 	UPROPERTY(EditAnywhere, Category = "Input")
 	UInputMappingContext* InputMappingContext;
 
@@ -40,13 +52,29 @@ protected:
 	UInputAction* MoveAction;
 	void Move(const FInputActionValue& Value);
 
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* AttackAction;
+	void Attack(const FInputActionValue& Value);
+	UFUNCTION()
+	void AttackFinished();
+
+	/*
+	* Animations
+	*/
 	UPROPERTY(EditAnywhere, Category = "Animation")
 	UPaperFlipbook* IdleAnimation;
 	UPROPERTY(EditAnywhere, Category = "Animation")
 	UPaperFlipbook* RunLeftAnimation;
 	UPROPERTY(EditAnywhere, Category = "Animation")
 	UPaperFlipbook* RunRightAnimation;
+	UPROPERTY(EditAnywhere, Category = "Animation")
+	TArray<UPaperFlipbook*> AttackAnimations;
+
 	void UpdateAnimation();
+	void IsMoving();
+
+	ECharacterDirection CharacterDirection = ECharacterDirection::ECD_Right;
+	ECharacterState CharacterState = ECharacterState::ECS_Idle;
 
 private:
 	UPROPERTY()
@@ -54,6 +82,4 @@ private:
 
 	UPROPERTY()
 	UCameraComponent* Camera;
-
-	ECharacterDirection CharacterDirection = ECharacterDirection::ECD_Right;
 };
